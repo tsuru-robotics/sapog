1099adbba54763c0915100d0ba89fb759d27990e
Silver Valdvee <silver_valdvee@hotmail.com>
2021-11-03 16:18:46 +0200

    Clean up allocator

--- firmware/tests/src/allocator.py
+++ firmware/tests/src/allocator.py
@@ -1,3 +1,4 @@
+"""For node id allocation in testing environments."""
 #
 # Copyright (c) 2021 Zubax, zubax.com
 # Distributed under the MIT License, available in the file LICENSE.
@@ -43,6 +44,8 @@ from pyuavcan.util import import_submodules, iter_descendants
 
 
 def make_handler_for_getinfo_update(allocator: Allocator, event: Optional[asyncio.Event] = None):
+    """Wraps the actual handler function that needs to have specific parameters and returns the handler with context."""
+
     def handle_getinfo_handler_format(node_id: int, previous_entry: Optional[Entry], next_entry: Optional[Entry]):
         async def handle_inner_function():
             if node_id:
@@ -56,65 +59,6 @@ def make_handler_for_getinfo_update(allocator: Allocator, event: Optional[asynci
         asyncio.get_event_loop().create_task(handle_inner_function())
 
     return handle_getinfo_handler_format
-
-
-def format_trace_view_nicely(trace: Trace, ids: typing.Dict[int, FixedPortObject]):
-    payload: str = ""
-    count = 0
-    deserialized = str(trace.transfer)
-    for memory_view in trace.transfer.fragmented_payload:
-        my_list = memory_view.tolist()
-        for byte in bytes(my_list):
-            payload += '{:02X} '.format(byte)
-        else:
-            payload = payload[:len(payload) - 1]
-        count += 1
-        if count >= 4:
-            payload += "\n"
-            count = 0
-        else:
-            payload += " | "
-    else:
-        payload = payload[:len(payload) - len(" |")]
-    deserialized = re.sub(r"fragmented_payload=\[[^\[\]]+?\]", "\nPAYLOAD\n" + payload, deserialized)
-    deserialized = deserialized.replace("AlienTransfer(AlienTransferMetadata(AlienSessionSpecifier(", "transfer(")[:-2]
-    for key, value in ids.items():
-        deserialized = deserialized.replace("subject_id=" + str(key), "subject_id=" + value.__name__ + f"({str(key)})")
-        deserialized = deserialized.replace("service_id=" + str(key), "service_id=" + value.__name__ + f"({str(key)})")
-    return deserialized
-
-
-def deserialize_trace(trace: Trace, ids: typing.Dict[int, FixedPortObject], subject_id: int):
-    obj = pyuavcan.dsdl.deserialize(ids[subject_id], trace.transfer.fragmented_payload)
-    return json.dumps(pyuavcan.dsdl.to_builtin(obj))
-
-
-def fill_ids():
-    ids = {}
-    filtered_types = ["ABCMeta"]
-    chained_descendants = chain(iter_descendants(pyuavcan.dsdl.FixedPortCompositeObject),
-                                iter_descendants(pyuavcan.dsdl.FixedPortServiceObject))
-    filtered_generator = (type_ for type_ in chained_descendants if type_ not in filtered_types)
-    for t in filtered_generator:
-        ids[pyuavcan.dsdl.get_fixed_port_id(t)] = t
-    return ids
-
-
-def make_capture_handler(tracer: Tracer, ids: typing.Dict[int, FixedPortObject]):
-    def capture_handler(capture: _tracer.Capture):
-        with open("rx_frm.txt", "a") as log_file:
-            # Checking to see if a transfer has finished, then assigning the value to transfer_trace
-            if (transfer_trace := tracer.update(capture)) is not None:
-                subject_id = None
-                try:
-                    subject_id = transfer_trace.transfer.metadata.session_specifier.data_specifier.subject_id
-                except Exception as e:
-                    pass
-                    # print(e.args[-1])
-                # print(deserialize_trace(transfer_trace, ids, subject_id))
-                log_file.write(format_trace_view_nicely(transfer_trace, ids) + "\n")
-
-    return capture_handler
 
 
 async def reset_node_id(sending_node: Node, current_target_node_id: int) -> bool:
@@ -131,11 +75,6 @@ async def reset_node_id(sending_node: Node, current_target_node_id: int) -> bool
 
 def configure_note_on_sapog(sending_node: Node, current_target_node_id: int):
     service_client = sending_node.make_client(uavcan.register.Access_1_0, current_target_node_id)
-
-
-def get_ids():
-    import_submodules(uavcan)
-    return fill_ids()
 
 
 hw_id_type = typing.Union[typing.List[int], bytes, bytearray]
@@ -220,13 +159,10 @@ async def make_complex_node(node_id: str,
     registry01["uavcan.can.iface"] = interface
     registry01["uavcan.can.mtu"] = int(mtu)
     registry01["uavcan.node.id"] = int(node_id)
-    ids = get_ids()
     node = make_node(NodeInfo(name=name), registry01)
     node.add_lifetime_hooks(lambda: print("starting"), lambda: print("closing"))
     node.start()
     tracer = node.presentation.transport.make_tracer()
-    if with_debugging and capture_handler_wrapper:
-        node.presentation.transport.begin_capture(capture_handler_wrapper(tracer, ids))
     node_tracker = NodeTracker(node)
     centralized_allocator = CentralizedAllocator(node)
     if get_info_handler_wrapper:
@@ -238,8 +174,7 @@ async def make_complex_node(node_id: str,
 async def run_allocator2(time_out: Optional[int] = None):
     event = asyncio.Event()
     complex_node = make_complex_node("com.zubax.sapog.tests.allocator",
-                                     get_info_handler_wrapper=make_handler_for_getinfo_update,
-                                     capture_handler_wrapper=make_capture_handler)
+                                     get_info_handler_wrapper=make_handler_for_getinfo_update)
 
     if time_out:
         async def time_out_coroutine(time_out_time):
