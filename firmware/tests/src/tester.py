58feee6b28ee473a8c2eb0c213adf299324dfcae
Silver Valdvee <silver_valdvee@hotmail.com>
2021-11-04 20:41:14 +0200

    Fixes

--- firmware/tests/src/tester.py
+++ firmware/tests/src/tester.py
@@ -82,7 +82,7 @@ async def get_any_target_node_id(test_conductor_node: Node):
     heartbeat_subscriber = test_conductor_node.make_subscriber(uavcan.node.Heartbeat_1_0)
     target_node_id = None
 
-    async def handle_heartbeats(message_class: MessageClass, transfer_from: pyuavcan.transport._transfer.TransferFrom):
+    def handle_heartbeats(message_class: MessageClass, transfer_from: pyuavcan.transport._transfer.TransferFrom):
         nonlocal target_node_id
         if transfer_from.source_node_id != test_conductor_node.id:
             target_node_id = transfer_from.source_node_id
@@ -92,7 +92,7 @@ async def get_any_target_node_id(test_conductor_node: Node):
     # stops here and waits for the handler to declare that it has received a fitting node_id
     # if the timeout sets the event first then None is returned
     # heartbeat_subscriber.close() This should be cleaned somewhere
-    event.wait()
+    await event.wait()
     return target_node_id
 
 
@@ -107,18 +107,15 @@ async def get_target_node_id_by_hw_id(test_conductor_node: Node, target_hw_id: h
         print("handler called")
         nonlocal target_node_id
         if transfer_from.source_node_id != test_conductor_node.id:
-            target_node_id = transfer_from.source_node_id
             get_info_client = test_conductor_node.make_client(uavcan.node.GetInfo_1_0, transfer_from.source_node_id)
             get_info_request = uavcan.node.GetInfo_1_0.Request()
             get_info_response: uavcan.node.GetInfo_1_0.Response = wrap_await(
                 asyncio.wait_for(get_info_client.call(get_info_request)), 1)
             if get_info_response.unique_id == target_hw_id:
+                target_node_id = transfer_from.source_node_id
                 event.set()
 
-            heartbeat_subscriber.receive_in_background(handle_heartbeats)
-            # stops here and waits for the handler to declare that it has received a fitting node_id
-            # if the timeout sets the event first then None is returned
-            # heartbeat_subscriber.close() This should be cleaned somewhere
+    heartbeat_subscriber.receive_in_background(handle_heartbeats)
 
     await event.wait()
 
