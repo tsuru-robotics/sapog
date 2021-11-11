0764802a2055b0a90bb55df52b8e74990631ac3f
Silver Valdvee <silver_valdvee@hotmail.com>
2021-11-11 13:46:26 +0200

    Fix long standing bug with inserting the path as a PosixPath rather than a string

--- firmware/tests/src/debugger.py
+++ firmware/tests/src/debugger.py
@@ -20,7 +20,9 @@ from pyuavcan.dsdl import FixedPortObject
 source_path = pathlib.Path(__file__).parent.absolute()
 dependency_path = source_path.parent / "deps"
 namespace_path = dependency_path / "namespaces"
-sys.path.insert(0, namespace_path.absolute())
+sys.path.insert(0, str(namespace_path.absolute()))
+print(f"Namespace path: {namespace_path}")
+print(f"sys.path: {sys.path}")
 
 import uavcan.pnp.NodeIDAllocationData_1_0
 import uavcan.node.ID_1_0
