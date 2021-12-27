13dfab03dd93a2d2d91257c200778ebe740505c7
Silver Valdvee <silver_valdvee@hotmail.com>
2021-12-27 19:37:09 +0200

    Progress

--- firmware/tests/src/utils.py
+++ firmware/tests/src/utils.py
@@ -51,6 +51,18 @@ def is_device_with_node_id_running(node_id):
         return True
 
 
+def restart_all_interfaces():
+    interfaces = get_available_slcan_interfaces()
+    for index, interface in enumerate(interfaces):
+        registry = make_registry(index, interfaces=[interface])
+        node = make_node(NodeInfo(name="com.zubax.sapog.tests.tester"), registry)
+        service_client = prepared_node.make_client(uavcan.node.ExecuteCommand_1_1, node_id)
+        msg = uavcan.node.ExecuteCommand_1_1.Request()
+        msg.command = msg.COMMAND_RESTART
+        response = wrap_await(service_client.call(msg))
+        assert response is not None
+
+
 def get_available_slcan_interfaces():
     result = subprocess.run(["netstat -i | tail -n +3 |cut -d\" \" -f1"], shell=True, stdout=subprocess.PIPE)
     list_of_interfaces = result.stdout.decode("utf-8").strip().split("\n")
@@ -74,24 +86,38 @@ def prepared_double_redundant_node():
 
 
 @pytest.fixture(scope="class")
-def get_interfaces_by_hw_id() -> typing.Dict[int, typing.List[str]]:
+def get_interfaces_by_hw_id(do_get_allocated_nodes: bool = False, do_get_unallocated_nodes: bool = True) -> typing.Dict[
+    int, typing.List[str]]:
     """
-    Makes a dictionary where every hw_id is a key and the value is a list of interfaces it communicates on.
+    Makes a dictionary where every hw_id or node_id is a key and the value is a list of interfaces it communicates on.
 
     This helps to automatically find out which devices are using which interfaces and then communicate to every device
     in a for loop to test these.
     """
     available_interfaces = get_available_slcan_interfaces()
-    interfaces_by_hw_id: typing.Dict[int, typing.List[str]] = {}
+    interfaces_by_hw_id: typing.Dict[int, typing.List[typing.Union[str, int]]] = {}
     for index, interface in enumerate(available_interfaces):
         registry = make_registry(index, interfaces=[interface])
         node = make_node(NodeInfo(name="com.zubax.sapog.tests.tester"), registry)
-        sub = node.make_subscriber(uavcan.pnp.NodeIDAllocationData_1_0)
-        received: uavcan.pnp.NodeIDAllocationData_1_0 = wrap_await(sub.receive_for(1.3))
-        if received.unique_id_hash in interfaces_by_hw_id.keys():
-            interfaces_by_hw_id[received.unique_id_hash].append(interface)
-        else:
-            interfaces_by_hw_id[received.unique_id_hash] = [interface]
+        done = False
+
+        def add_to_dictionary_list(key, value):
+            """Used twice below"""
+            if received.unique_id_hash in interfaces_by_hw_id.keys():
+                interfaces_by_hw_id[key].append(value)
+            else:
+                interfaces_by_hw_id[key] = [value]
+
+        if do_get_unallocated_nodes:
+            sub = node.make_subscriber(uavcan.pnp.NodeIDAllocationData_1_0)
+            received: uavcan.pnp.NodeIDAllocationData_1_0 = wrap_await(sub.receive_for(1.3))
+            add_to_dictionary_list(received.unique_id_hash, interface)
+        if not done and do_get_allocated_nodes:
+            sub = node.make_subscriber(uavcan.node.Heartbeat_1_0)
+            received_tuple: typing.Tuple[uavcan.node.Heartbeat_1_0, pyuavcan.transport.TransferFrom] = wrap_await(
+                sub.receive_for(1.3))
+            heartbeat, transfer_from = received_tuple
+            add_to_dictionary_list(transfer_from.source_node_id, interface)
     return interfaces_by_hw_id
 
 
