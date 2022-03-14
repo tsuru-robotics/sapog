251377b47e4ee0086876f46fa0992bb1e55eb22e
Silver Valdvee <silver_valdvee@hotmail.com>
2022-03-14 14:14:03 +0200

    Update

--- firmware/src/fifo_queue.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2022 Zubax, zubax.com
- * Distributed under the MIT License, available in the file LICENSE.
- * Author: Silver Valdvee <silver.valdvee@zubax.com>
- */
-#include "fifo_queue.hpp"
